218a0c438e487959144985e3e93c4e77565bb8af
Dev Js <alienwaredev369@outlook.com>
2026-03-16 22:21:33 -0300

    implementando javascript

---
+++ script.js
@@ -0,0 +1,13 @@
+const convertButton = document.querySelector("convert-button")
+
+function convertValues() {
+    const valorInicial = document.querySelector(".moeda-entrada").value
+
+    const dolarToday = 5.2
+
+    const convertValor = valorInicial / dolarToday
+
+    console.log(convertValor)
+} 
+
+convertButton.addEventListener("click", convertValues)
