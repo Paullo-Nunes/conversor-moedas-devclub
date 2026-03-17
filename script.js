d67ddcfcff0da09cb8a1957a7b28b8e9411adc3e
Dev Js <alienwaredev369@outlook.com>
2026-03-17 09:55:49 -0300

    mapeamento dos valores

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const convertButton = document.querySelector(".convert-button")
 function convertValues() {
     const valorInicial = document.querySelector(".valor-moeda").value
 
+    const valorParaConverter = document.querySelector(".moeda-entrada") //moeda Real
+
+    const valorConvertido = document.querySelector(".moeda-saida") //outras moedas
+
     const dolarToday = 5.2
 
     const convertValor = valorInicial / dolarToday
